5ecb0ffe4ca9f9f60b02f92f79828449c09d8864
Alejandro <ale_doblas@hotmail.com>
2023-11-05 19:27:22 +0100

    Environments variables fix

--- scripts/set-envs.js
+++ scripts/set-envs.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const targetPath = './src/environments/environment.ts';
 const customVariables = Object.fromEntries(Object.entries(process.env).filter(([k,v]) => k.includes('ZZ_')));
-const envFileContent = `export const environment = ${ customVariables }`;
+const envFileContent = `export const environment = ${ JSON.stringify(customVariables) }`;
 
 mkdirSync('./src/environments', { recursive: true });
 
